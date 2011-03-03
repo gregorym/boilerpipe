f4a8acf733b305017a44c9c1905f264639fca3ef
Grégory Marcilhacy <g.marcilhacy@gmail.com>
2011-03-03 21:16:51 +0100

    Boilerpipe api

---
+++ boilerpipe.rb
@@ -0,0 +1,26 @@
+require 'open-uri'
+
+class Object
+  def blank?
+    respond_to?(:empty?) ? empty? : !self
+  end
+  
+  def present?
+    !blank?
+  end
+end
+
+module BoilerPipe
+  DEFAULT_API_URL = 'http://boilerpipe-web.appspot.com/extract'
+  EXTRACTORS = [ :ArticleExtractor, :DefaultExtractor, :LargestContentExtractor, :KeepEverythingExtractor, :CanolaExtractor ]
+  OUTPUT_FORMATS =  [ :html, :htmlFragment, :text, :json, :debug ]
+  
+  def self.extract(extract_url, opts = {})
+    @output    = opts[:output].present?     ? opts[:output] : OUTPUT_FORMATS.first
+    @extractor = opts[:extractor].present?  ? opts[:output] : EXTRACTORS.first 
+    @api       = opts[:api].present?        ? opts[:api]    : DEFAULT_API_URL
+    url = [@api, "?url=#{extract_url}", "&extractor=#{@extractor}","&output=#{@output}"].join
+    resp = open(url)
+    print resp.read
+  end
+end
